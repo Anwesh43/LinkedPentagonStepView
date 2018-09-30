a2da16e9f856524174eb11f878a14b20e0ef3c87
Anwesh43 <anweshthecool0@gmail.com>
2018-10-01 02:21:33 +0530

    adding pentagonstepview create method

--- app/src/main/java/com/anwesh/uiprojects/linkedpentagonstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedpentagonstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedpentagonstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.pentagonstepview.PentagonStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PentagonStepView.create(this)
     }
 }
