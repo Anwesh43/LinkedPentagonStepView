182ad20f4b38523d1ffbd34ec50542c4adf7ae7d
Anwesh43 <anweshthecool0@gmail.com>
2018-10-01 01:06:24 +0530

    adding linbbraries and defining nodes

--- pentagonstepview/src/main/java/com/anwesh/uiprojects/pentagonstepview/PentagonStepView.kt
+++ pentagonstepview/src/main/java/com/anwesh/uiprojects/pentagonstepview/PentagonStepView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.pentagonstepview
 /**
  * Created by anweshmishra on 01/10/18.
  */
+
+import android.view.MotionEvent
+import android.view.View
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
