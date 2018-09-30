e74b96125fb6e2aca3389c4300e700000718bd9e
Anwesh43 <anweshthecool0@gmail.com>
2018-10-01 02:01:49 +0530

    adding drawPSNode canvas extension method

--- pentagonstepview/src/main/java/com/anwesh/uiprojects/pentagonstepview/PentagonStepView.kt
+++ pentagonstepview/src/main/java/com/anwesh/uiprojects/pentagonstepview/PentagonStepView.kt
@@ -14,6 +14,30 @@ import android.graphics.Color
 val nodes : Int = 5
 val size : Int = 5
 
+fun Canvas.drawPSNode(i : Int, scale : Float, paint : Paint) {
+    paint.strokeCap = Paint.Cap.ROUND
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.color = Color.parseColor("#01579B")
+    val r : Float = gap / 3
+    val deg : Float = 360f / size
+    val fact : Float = 1f / size
+    save()
+    translate(w/2, i * gap + gap)
+    for (j in 0..size - 1) {
+        val sc : Float = Math.min(fact, Math.max(0f, scale - j * fact)) * size
+        val x : Float = r * Math.sin((deg/2) * Math.PI/180).toFloat()
+        val y : Float = r * Math.cos((deg/2) * Math.PI/180).toFloat()
+        save()
+        rotate(deg * j)
+        drawLine(x, y, x - 2 * x * sc, y, paint)
+        restore()
+    }
+    restore()
+}
+
 class PentagonStepView (ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
