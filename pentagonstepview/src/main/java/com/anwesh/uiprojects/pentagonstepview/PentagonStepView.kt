39194f03cbfe8b69ae321d43070e331723e3a55e
Anwesh43 <anweshthecool0@gmail.com>
2018-10-01 01:08:02 +0530

    adding PentagonStepView class

--- pentagonstepview/src/main/java/com/anwesh/uiprojects/pentagonstepview/PentagonStepView.kt
+++ pentagonstepview/src/main/java/com/anwesh/uiprojects/pentagonstepview/PentagonStepView.kt
@@ -12,3 +12,21 @@ import android.graphics.Canvas
 import android.graphics.Color
 
 val nodes : Int = 5
+
+class PentagonStepView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
