d0bbf444d888df5005eacb5b7e4f068052bb8e94
Anwesh43 <anweshthecool0@gmail.com>
2018-10-01 02:26:15 +0530

    fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedpentagonstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedpentagonstepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedpentagonstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.pentagonstepview.PentagonStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         PentagonStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
